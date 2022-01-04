aea77639066ac191e6c7134731ca11190d679fe2
Hassan Moussa <hassanmoussa@lereum.com>
2022-01-04 15:54:42 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,15 +11,12 @@ import (
 func main() {
 
 	// add the Amazon Keyspaces service endpoint
-	cluster := gocql.NewCluster("cassandra.eu-west-1.amazonaws.com:9142")
+	cluster := gocql.NewCluster("18.202.32.58:9042")
 	// add your service specific credentials
 	cluster.Authenticator = gocql.PasswordAuthenticator{
 		Username: "cdk-at-651604682962",
 		Password: "WnMqn7uW0Q7iJZtWio202rJpRslH3yNnmUk8TYx5YFo="}
-	// provide the path to the sf-class2-root.crt
-	cluster.SslOpts = &gocql.SslOptions{
-		CaPath: "sf-class2-root.crt",
-	}
+
 	// Override default Consistency to LocalQuorum
 	cluster.Consistency = gocql.LocalQuorum
 	// Disable initial host lookup
@@ -28,10 +25,19 @@ func main() {
 	if err != nil {
 		fmt.Println("err>", err)
 	}
+
 	defer session.Close()
+	iter := session.Query("CREATE KEYSPACE IF NOT EXISTS bitsouks_test_table WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};").Iter()
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+	iter = session.Query(" CREATE TABLE IF NOT EXISTS bitsouks_test_table.user_table (id int,email text,age int,PRIMARY KEY (id));").Iter()
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
 	//add data to table
 	t1 := time.Now()
-	iter := session.Query("INSERT INTO bitsouks_test_table.user_table (id, email, age) VALUES (1, 'john@example.com', 21);").Iter()
+	iter = session.Query("INSERT INTO bitsouks_test_table.user_table (id, email, age) VALUES (1, 'john@example.com', 21);").Iter()
 	t2 := time.Now()
 	diff := t2.Sub(t1)
 	fmt.Println("TIME TO Add", diff)
